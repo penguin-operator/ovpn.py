a770b76ed5e29256f163d26d93ca22ae1f0f9730
penguin-operator <ekuzminskij594@gmail.com>
2024-12-27 15:42:01 +0300

    fixed timeouts errors and user-agent bugs

--- ovpn.py
+++ ovpn.py
@@ -14,7 +14,7 @@ cache = {
 	"ips": list[str](),
 }
 headers = {
-	"user-agent": "curl/8.11.1",
+	"user-agent": "Firefox/136.0",
 }
 
 def geo_format(geo: dict[str, str | float | bool]) -> dict[str, str | float | bool]:
@@ -23,7 +23,7 @@ def geo_format(geo: dict[str, str | float | bool]) -> dict[str, str | float | bo
 	geo["city"] = geo["city"].replace(' ', '_').lower()
 	return geo
 
-async def get_ip_geo(client: httpx.AsyncClient, ip: str, *, sleep: float = 10) -> dict[str, float | str | bool]:
+async def get_ip_geo(client: httpx.AsyncClient, ip: str, *, sleep: float = 5) -> dict[str, float | str | bool]:
 	if ip in cache["ip_geo"].keys(): return cache["ip_geo"][ip]
 	while True:
 		try:
@@ -33,14 +33,20 @@ async def get_ip_geo(client: httpx.AsyncClient, ip: str, *, sleep: float = 10) -
 		except httpx.HTTPError:
 			await asyncio.sleep(sleep)
 
-async def download(client: httpx.AsyncClient, path: str, url: str):
-	name = url.split('/')[-1]
-	geo, file = await asyncio.gather(get_ip_geo(client, name.split('_')[0]), client.get(url))
-	geo = geo_format(geo)
-	path = path.format(country=geo["country"],region=geo["regionName"],city=geo["city"])
-	await aiofiles.os.makedirs(path, exist_ok=True)
-	async with aiofiles.open(path + os.sep + name, mode='w') as handle:
-		await handle.write(file.text)
+async def download(client: httpx.AsyncClient, path: str, url: str, *, sleep: float = 10):
+	while True:
+		try:
+			name = url.split('/')[-1]
+			geo, file = await asyncio.gather(get_ip_geo(client, name.split('_')[0]), client.get(url))
+			if file.status_code != 200: return
+			geo = geo_format(geo)
+			path = path.format(country=geo["country"],region=geo["regionName"],city=geo["city"])
+			await aiofiles.os.makedirs(path, exist_ok=True)
+			async with aiofiles.open(path + os.sep + name, mode='w') as handle:
+				await handle.write(file.text)
+			return
+		except httpx.HTTPError:
+			await asyncio.sleep(sleep)
 
 async def check(path: str, *queries: str):
 	queries = {q.split('=')[0]: q.split('=')[1] for q in queries}
@@ -48,7 +54,7 @@ async def check(path: str, *queries: str):
 	region = queries.get("region")
 	city = queries.get("city")
 	tasks = []
-	async with httpx.AsyncClient(headers=headers) as client:
+	async with httpx.AsyncClient(headers=headers, timeout=60) as client:
 		for root, folder, files in os.walk(path):
 			for file in files:
 				ip = file.split('_')[0]
@@ -58,21 +64,19 @@ async def check(path: str, *queries: str):
 		geos = await asyncio.gather(*tasks)
 	for geo in geos:
 		geo = geo_format(geo)
-		try:
-			if country == geo["country"] or region == geo["regionName"] or city == geo["city"]:
-				print(geo["query"])
-		except KeyError: pass
+		if country == geo["country"] or region == geo["regionName"] or city == geo["city"]:
+			print(geo["query"])
 
 async def get(url: str, path: str):
 	url = urlparse(url)
 	tasks = []
-	async with httpx.AsyncClient(headers=headers) as client:
+	async with httpx.AsyncClient(headers=headers, timeout=60) as client:
 		page = await client.get(url.geturl())
 		soup = bs4.BeautifulSoup(page.text, "lxml")
 		for link in soup.find_all('a'):
 			link = urlparse(link.get("href"))._replace(scheme = url.scheme, netloc = url.netloc)
 			if link.path.endswith(".ovpn"): tasks += [download(client, path, link.geturl())]
-		tasks = await asyncio.gather(*tasks)
+		await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
 	match sys.argv[1:]:
